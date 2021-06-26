2c67d75ceeca3550fb1eceb3567b3133228970ad
phunvtgcd18380 <66460872+phunvtgcd18380@users.noreply.github.com>
2021-06-26 15:49:12 +0700

    Add project files.

---
+++ RazorDemoApp/Migrations/20210616094228_ISBNCol.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorDemoApp.Migrations
+{
+    public partial class ISBNCol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
